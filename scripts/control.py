10a2825504658dca91c0fa0642c81432c66950ec
alk15 <alex13kouris@hotmail.com>
2024-10-10 14:20:51 +0100

    Clean up control.py

--- scripts/control.py
+++ scripts/control.py
@@ -23,61 +23,20 @@ def move_home():
                    -2.2097670971912553]
     panda.move_to_configuration(home_config)
 
-def launch_particle_filtering():
-    global particle_filter_running
-
-    if particle_filter_running: # Killing previous process isn't finished yet; wait.
-        print('Particle filtering already running')
-        return
-    
-    particle_filter_running = True
-    uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
-    roslaunch.configure_logging(uuid)
-    path = os.path.expanduser('~/catkin_ws/src/PBPF')
-    launch = roslaunch.parent.ROSLaunchParent(uuid, [f'{path}/launch/code_visuial.launch'])
-    launch.start()
-    time.sleep(2)
-    manage_windows()
-
-def manage_windows():
-    commands = [
-        'wmctrl -i -r $(wmctrl -l | grep "/dope/rgb_points_my_wireframe" | awk "{print $1}") -e "0,0,0,960,1080"',
-        'wmctrl -i -r $(wmctrl -l | grep "Bullet" | sed -n 1p | awk "{print $1}") -e "0,960,0,960,500"',
-        'wmctrl -i -r $(wmctrl -l | grep "Bullet" | sed -n 2p | awk "{print $1}") -e "0,960,510,960,500"',
-    ]
-
-    for command in commands:
-        subprocess.Popen(command, shell=True)
-
-def kill_particle_filtering():
-    global particle_filter_running
-    if particle_filter_running:
-        print('Killing particle filter')
-        os.system('pkill -9 Visualisation')
-        os.system('pkill -9 Physics_Based_')
-        os.system('pkill -9 image_view')
-        particle_filter_running = False
-
 def joystick_callback(data):
-    global start_particle_filter, particle_filter_running
 
-    if data.buttons[KEYMAP['start']]:
-        if not particle_filter_running:
-            start_particle_filter = True
-    elif data.buttons[KEYMAP['select']]:
-        kill_particle_filtering()
-    elif data.buttons[KEYMAP['x']]:
+    if data.buttons[KEYMAP['x']]:
         panda.fully_close_gripper()
     elif data.buttons[KEYMAP['o']]:
         panda.fully_open_gripper()
-    elif data.buttons[KEYMAP['up-arrow']]:
-        move_gripper(x=0.03)
-    elif data.buttons[KEYMAP['down-arrow']]:
-        move_gripper(x=-0.03)
-    elif data.buttons[KEYMAP['left-arrow']]:
-        move_gripper(y=0.03)
-    elif data.buttons[KEYMAP['right-arrow']]:
-        move_gripper(y=-0.03)
+    #elif data.buttons[KEYMAP['up-arrow']]:
+    #    move_gripper(x=0.03)
+    #elif data.buttons[KEYMAP['down-arrow']]:
+    #    move_gripper(x=-0.03)
+    #elif data.buttons[KEYMAP['left-arrow']]:
+    #    move_gripper(y=0.03)
+    #elif data.buttons[KEYMAP['right-arrow']]:
+    #    move_gripper(y=-0.03)
     elif data.buttons[KEYMAP['l1']]:
         move_gripper(z=0.01)
     elif data.buttons[KEYMAP['r1']]:
@@ -86,16 +45,11 @@ def joystick_callback(data):
         move_home()
 
 if __name__ == '__main__':
-    rospy.init_node('panda-ps3-controller')
-    particle_filter_running = False
-    start_particle_filter = False
+    rospy.init_node('panda_ps_control')
 
     panda = Panda()
     rospy.Subscriber('joy', Joy, joystick_callback, queue_size=1)
 
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
-        if start_particle_filter:
-            launch_particle_filtering()
-            start_particle_filter = False
         rate.sleep()
